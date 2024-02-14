1498182402318a0df034095f5abbd018900e10e6
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-02-15 02:28:47 +0600

    modal update

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -6,15 +6,19 @@ const NewsDetails = ({ isOpen, onClose, data }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-70 z-50">
-      <div className="bg-white p-4 max-w-xl w-70vh rounded-lg shadow-lg">
+      <div className="bg-white p-5 max-h-screen rounded-lg max-w-screen-md marker:shadow-lg overflow-y-auto">
         <button
           className="mb-5 border px-2 rounded-md hover:bg-red-500"
           onClick={onClose}
         >
-          X
+          Back
         </button>
         <div className="col-span-12 mb-6 md:col-span-8">
-          <img className="w-full" src={data.urlToImage} alt="no image" />
+          <img
+            className="w-full rounded-lg"
+            src={data.urlToImage}
+            alt="no image"
+          />
           <div className="col-span-12 mt-6 md:col-span-4">
             {data.author && <p className="mb-2">Author: {data.author}</p>}
             <h3 className="mb-2.5 text-xl font-bold lg:text-[20px]">
